1517aeb7e7239dc3fdb506a80cbec245f93827f1
David Gross <dgroseph@gmail.com>
2019-04-30 12:18:08 -0700

    2nd draft of opsimaths layout
     - out in the wild, but
     - compiled from raw keymap.c from opsimaths.json at config.qmk.fm

--- keyboards/atreus62/keymaps/opsimaths/keymap.c
+++ keyboards/atreus62/keymaps/opsimaths/keymap.c
@@ -1,55 +1,56 @@
 #include QMK_KEYBOARD_H
 
-#include "action_layer.h"
-
 #define OOOOOOO KC_TRNS
+
+#define SFTPGUP LSFT_T(KC_PGUP)
+#define CTLPGDN LCTL_T(KC_PGDN)
+#define LGUI_HM LGUI_T(KC_HOME)
+#define LALT_ND LALT_T(KC_END)
+
+#define RSFT_UP RSFT_T(KC_UP)
+#define RCTL_DN RCTL_T(KC_DOWN)
+#define RALT_LF RALT_T(KC_LEFT)
+#define RGUI_RT RGUI_T(KC_RGHT)
+
+#define LT1_ENT LT(1,KC_ENT)
+#define LT2_SPC LT(2,KC_SPC)
+#define LT3BSPC LT(3,KC_BSPC)
+
+#define OSXVOLU KC__VOLUP
+#define OSXVOLD KC__VOLDOWN
+
+LGUI_T(KC_HOME)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,                      KC_6,   KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,                      KC_Y,   KC_U,  KC_I,    KC_O,   KC_P,    KC_BSLS,
-    KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,                      KC_H,   KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_LSPO, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,                      KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
-    KC_LCTL, KC_LGUI, KC_LALT, MO(3), MO(1), KC_BSPC, KC_RCTL, KC_ENT,  KC_SPC, MO(2), KC_DEL,  KC_EQL, KC_LBRC, KC_RBRC 
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, 
+    KC_LBRC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, 
+    KC_EQL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
+    SFTPGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_UP, 
+    CTLPGDN, LGUI_HM, LALT_ND, LT3BSPC, LT2_SPC, LT1_ENT, KC_TAB,  KC_BSLS, KC_ENT,  KC_SPC,  KC_DEL,  RALT_LF, RGUI_RT, RCTL_DN
   ),
 
   [1] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                   _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______,
-    _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,                   _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______,
-    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                   _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PGUP,
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                   _______, KC_P0,   KC_PDOT, KC_PENT, KC_PPLS, KC_PGDN,
-    _______, _______, _______, _______, OOOOOOO, _______, _______, _______, _______, _______, _______, _______, _______, _______ 
+    _______, _______, _______, _______, _______, _______,                   KC_CALC, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_BSPC, 
+    _______, KC_F9,  KC_F10,   KC_F11,  KC_F12,  _______,                   _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______, 
+    _______, KC_F5,  KC_F6,    KC_F7,   KC_F8,   _______,                   _______, KC_P4,   KC_P5,   KC_P6,   KC_PCMM, _______, 
+    _______, KC_F1,  KC_F2,    KC_F3,   KC_F4,   _______,                   _______, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, _______, 
+    _______, _______, _______, _______, OOOOOOO, _______, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, KC_PENT, _______
   ),
 
   [2] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,                   _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______,
-    _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,                   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-    _______, _______, KC_MPLY, KC_MRWD, KC_MFFD, _______,                   _______, KC_BTN1, KC_BTN2, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, OOOOOOO, _______, _______, _______, _______ 
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, KC_WAKE, KC_SLEP, KC_PWR, 
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, 
+    _______, _______, KC_MSTP, OSXVOLU, KC_MPLY, _______,                   _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, 
+    _______, _______, KC_MRWD, OSXVOLD, KC_MFFD, _______,                   _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, 
+    _______, _______, _______, OOOOOOO, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [3] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______,  _______,  _______,
-    _______, _______, _______, _______, _______, _______,                   KC_COPY, KC_PGUP, _______, _______,  KC_PASTE, KC_DEL,
-    _______, _______, _______, KC_PGDN, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,  _______,
-    _______, _______, KC_CUT,  _______, _______, _______,                   _______, _______, _______, _______,  _______,  _______,
-    _______, _______, _______, OOOOOOO, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______ 
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+    _______, _______, OOOOOOO, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ 
   )
 };
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  // MACRODOWN only works in this function
-  switch (id) {
-  case 0:
-    if (record->event.pressed) {
-      register_code(KC_RSFT);
-    }
-    else {
-      unregister_code(KC_RSFT);
-    }
-    break;
-  }
-  return MACRO_NONE;
-};
-
